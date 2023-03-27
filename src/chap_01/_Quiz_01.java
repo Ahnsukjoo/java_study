331a8f7f6af3d75fbf7e7328fe7a00eaca00fb7d
Ahnsukjoo <110189683+Ahnsukjoo@users.noreply.github.com>
2023-03-28 01:18:39 +0900

    quiz

---
+++ src/chap_01/_Quiz_01.java
@@ -0,0 +1,15 @@
+package chap_01;
+
+public class _Quiz_01 {
+
+    public static void main(String[] args) {
+        int busNo = 1234;
+        int minute = 3;
+        double Km = 1.5;
+
+        System.out.println(
+            "잠시후 버스번호 " + busNo + "버스가 " + minute + "분 후 도착예정입니다. 남은 거리는 " + Km + "만큼 남았습니다.");
+
+    }
+
+}
