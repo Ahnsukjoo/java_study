2bc2219243257afa86c62ccdcb2572122ab4c4c7
Ahnsukjoo <110189683+Ahnsukjoo@users.noreply.github.com>
2023-03-28 01:17:57 +0900

    constants

---
+++ src/chap_01/_06_Constants.java
@@ -0,0 +1,17 @@
+package chap_01;
+
+import com.sun.xml.internal.ws.api.ha.StickyFeature;
+import java.sql.SQLOutput;
+import org.omg.PortableInterceptor.ServerRequestInfo;
+
+public class _06_Constants {
+
+    public static void main(String[] args) {
+        final String KR_COUNTRY_CODE = "+82"; //국가번호 (빨리)
+        //KR_COUNTRY_CODE =  "+8282";
+        System.out.println(KR_COUNTRY_CODE);
+
+        final double PI = 3.141592; //원주율
+        final String DATE_OF_BIRTH = "2001-12-31"; //생년 월일
+    }
+}
