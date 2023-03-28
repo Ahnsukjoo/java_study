e2bbfa006dcefb0357b4e0fb750ff917bb358cae
Ahnsukjoo <110189683+Ahnsukjoo@users.noreply.github.com>
2023-03-29 04:23:25 +0900

    Quiz3

---
+++ src/chap_03/_Quiz_03.java
@@ -0,0 +1,13 @@
+package chap_03;
+
+public class _Quiz_03 {
+
+    public static void main(String[] args) {
+        String no1 = "901231-1234567";
+        String no2 = "030708-4567890";
+
+        System.out.println(no1.substring(0, 8));
+        System.out.println(no2.substring(0, 8));
+    }
+
+}
