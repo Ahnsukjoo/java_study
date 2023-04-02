758a3dee2c5ff4c108dacb8ae25bcc1850815753
Ahnsukjoo <110189683+Ahnsukjoo@users.noreply.github.com>
2023-04-03 04:06:57 +0900

    for

--- src/chap_04/_05_For.java
+++ src/chap_04/_05_For.java
@@ -29,7 +29,30 @@ public class _05_For {
             //System.out.println("환영합니다. 나코입니다." + i);
             System.out.println("환영합니다. 코나입니다." + i);
         }
-
+        //짝수만 출력 (fori 만 적고 엔터)
+        // 0 , 2 , 4 , 6 , 8
+        for (int i = 0; i < 10; i += 2) {
+            System.out.print(i);
+        }
+        //홀수만 출력
+        System.out.println();
+        for (int i = 1; i < 10; i += 2) {
+            System.out.print(i);
+        }
+        System.out.println();
+        //거꾸로 숫자
+        for (int i = 10; i > 0; i -= 2) {
+            System.out.print(i);
+        }
+        //1 부터 10까지의 수들의 합
+        // 1+ 2 + ... + 10 =55
+        System.out.println();
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
+            System.out.println("현재까지의 총합은" + sum + "입니다.");
+        }
+        System.out.println("1부터 10까지의 모든 수의 총합은 " + sum + "입니다.");
     }
 
 }
