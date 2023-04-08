0ce9fe12fc11a3d94e7a137d7704f93f644e1113
Ahnsukjoo <110189683+Ahnsukjoo@users.noreply.github.com>
2023-04-08 23:48:18 +0900

    MultiArrayloop

---
+++ src/chap_05/_04_MultiArrayLoop.java
@@ -0,0 +1,40 @@
+package chap_05;
+
+public class _04_MultiArrayLoop {
+
+    public static void main(String[] args) {
+        // 다차원 배열의 순회
+        String[][] seats = new String[][]{
+            {"A1", "A2", "A3", "A4", "A5"},
+            {"B1", "B2", "B3", "B4", "B5"},
+            {"C1", "C2", "C3", "C4", "C5"}
+        };
+
+        for (int i = 0; i < 3; i++) { // 세로
+            for (int j = 0; j < 5; j++) {
+                System.out.print(seats[i][j]+" ");
+
+            }
+            System.out.println();
+
+        }
+        System.out.println("---------------------------");
+
+        String[][] seats2 = new String[][]{
+            {"A1", "A2", "A3"},
+            {"B1", "B2", "B3", "B4"},
+            {"C1", "C2", "C3", "C4", "C5"}
+        };
+
+        for (int i = 0; i < seats2.length; i++) { // 세로
+            for (int j = 0; j < seats2[i].length; j++) {
+                System.out.print(seats2[i][j]+" ");
+
+            }
+            System.out.println();
+
+        }
+
+    }
+
+}
