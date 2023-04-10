f8e4d2edf02675c3e634315be18843935c28d766
Ahnsukjoo <110189683+Ahnsukjoo@users.noreply.github.com>
2023-04-10 20:21:57 +0900

    Quiz

---
+++ src/chap_05/_Quiz_05.java
@@ -0,0 +1,14 @@
+package chap_05;
+
+public class _Quiz_05 {
+
+    public static void main(String[] args) {
+        String[] shoose = {"250", "255", "260", "265", "270", "275", "280", "285", "290", "295"};
+        for (int i = 0; i <9; i++) {
+
+            System.out.println("사이즈 " + shoose[i] + "(재고있음)");
+        }
+
+    }
+
+}
