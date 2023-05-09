25a0e4cbba1ce18c8affa48ab1f2f0bf338bb258
Ahnsukjoo <110189683+Ahnsukjoo@users.noreply.github.com>
2023-05-10 00:05:47 +0900

    VariableScope

--- src/chap_06/_07_VariableScope.java
+++ src/chap_06/_07_VariableScope.java
@@ -2,18 +2,18 @@ package chap_06;
 
 public class _07_VariableScope {
 
-    public static void methodA() {
-        System.out.println(number);
-    }
-
-    public static void methodB() {
-        int result = 1;
-    }
-
-    public static void main(String[] args) {
-        int number = 3;
-
-        System.out.println(result);
-    }
+//    public static void methodA() {
+//        System.out.println(number);
+//    }
+//
+//    public static void methodB() {
+//        int result = 1;
+//    }
+//
+//    public static void main(String[] args) {
+//        int number = 3;
+//
+//        System.out.println(result);
+//    }
 
 }
